feb29be911b6a958a305bd760f5871157110f50a
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-17 16:47:48 +0200

    removed if-statement

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,18 +3,15 @@ int	ft_strlcpy(char *dst, const char *src, int dstsize)
 	int	len;
 
 	len = 0;
-	if (dstsize > 0)
+	while (src[len] != '\0')
 	{
-		while (src[len] != '\0')
+		if (len == dstsize)
 		{
-			if (len == dstsize)
-			{
-				len--;
-				break;
-			}
-			dst[len] = src[len];
-		 	len++;
+			len--;
+			break;
 		}
+		dst[len] = src[len];
+		len++;
 	}
 	dst[len] = '\0';
 	while (src[len] != '\0')
