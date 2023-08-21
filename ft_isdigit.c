b2667a1824933c181b9e45c2a40d14579d76d898
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-21 14:16:17 +0200

    finish ft_isdigit.c and add main

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,20 +1,34 @@
-int	ft_isprint(int c)
+int	ft_isdigit(int	c)
 {
-	if (c >= 32 && c <= 126)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
 }
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <ctype.h>
 
-int	main(void)
-{
-	int	c = 46;
+#define MAX_STRING_LENGTH 1
+#define NUM_STRINGS 20
 
-	printf("%d\n", isprint(c));
-	printf("%d\n", ft_isprint(c));
-	
-	return (0);
+int main()
+{
+    srand(time(NULL));
+    char    c;
+    for (int loop = 0; loop < NUM_STRINGS; ++loop)
+    {
+        c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw,?;.:/=$0123456789"[rand() % 70];
+        printf("-------------------------------------------------\n");
+        printf("RANDOM CHARACTER %d: %d\n", loop + 1, c);    
+        printf("ft_isdigit: %d\n", ft_isdigit(c));
+	printf("isdigit: %d\n", isdigit(c));
+        printf("-------------------------------------------------\n");
+        printf("\n\n\n\n\n");
+        
+    }
+
+    return 0;
 }
