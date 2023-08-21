332362df564b7e3a462cd9c3b5cda3a7936eb142
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-21 13:37:56 +0200

    finish ft_tolower.c and added main

--- ft_tolower.c
+++ ft_tolower.c
@@ -4,3 +4,30 @@ int	ft_tolower(int c)
 		c +=32;
 	return (c);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+#define MAX_STRING_LENGTH 1
+#define NUM_STRINGS 5
+
+int main()
+{
+    srand(time(NULL));
+    char    c;
+    for (int loop = 0; loop < NUM_STRINGS; ++loop)
+    {
+        c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw,?;.:/=$"[rand() % 60];
+        printf("-------------------------------------------------\n");
+        printf("RANDOM CHARACTER %d: %d\n", loop + 1, c);    
+        printf("ft_tolower: %d\n", ft_tolower(c));
+	printf("tolower: %d\n", tolower(c));
+        printf("-------------------------------------------------\n");
+        printf("\n\n\n\n\n");
+        
+    }
+
+    return 0;
+}
