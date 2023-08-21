4414d8e1a6b49a8a7558922b94989748b57b2447
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-21 11:13:25 +0200

    update ft_toupper.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -4,3 +4,33 @@ int	ft_toupper(int c)
 		c -=32;
 	return (c);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+#define MAX_STRING_LENGTH 1
+#define NUM_STRINGS 5
+
+int main()
+{
+    srand(time(NULL));
+    char    c;
+
+    for (int loop = 0; loop <= NUM_STRINGS; ++loop)
+    {
+        c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw,?;.:/=$"[random () % 60];
+    
+        printf("-------------------------------------------------\n");
+        printf("RANDOM CHARACTER %d: %d\n", loop + 1, c);    
+        printf("ft_toupper: %d\n", ft_toupper(c));
+	printf("toupper: %d\n", toupper(c));
+        printf("-------------------------------------------------\n");
+        printf("\n\n\n\n\n");
+        
+    }
+
+    return 0;
+}
+
