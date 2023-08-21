72afb3c077711c22656becfab03da6847fa1b633
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-21 17:14:23 +0200

    finish ft_isalpha.c and add main

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -5,3 +5,31 @@ int	ft_isalpha(int	c)
 	else
 		return (0);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+#define MAX_STRING_LENGTH 1
+#define NUM_STRINGS 20
+
+int main()
+{
+    srand(time(NULL));
+    char    c;
+    for (int loop = 0; loop < NUM_STRINGS; ++loop)
+    {
+        c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw,?;.:/=$0123456789"[rand() % 70];
+        printf("-------------------------------------------------\n");
+        printf("RANDOM CHARACTER %d: %d\n", loop + 1, c);
+        printf("ft_isalpha: %d\n", ft_isalpha(c));
+	printf("isalpha: %d\n", isalpha(c));
+        printf("-------------------------------------------------\n");
+        printf("\n\n\n\n\n");
+        
+    }
+
+    return 0;
+
+}
