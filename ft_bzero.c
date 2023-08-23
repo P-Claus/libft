fa4e592ef6b57dc1f0f49bad3fc2eab8b525f085
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-23 09:48:10 +0200

    create ft_bzero.c

---
+++ ft_bzero.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+void    ft_bzero(void *s, size_t n)
+{
+    size_t  counter;
+    
+    counter = 0;
+    while (counter < n)
+        *(char *)(s + counter++) = 0;
+}
+
+#include <stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char src1[21] = "gdfhjgazGHJGgdhsqjjG";
+    char src2[21] = "gdfhjgazGHJGgdhsqjjG";
+    printf("Before the function: %s\n", src1);
+    printf("-------------------------------------------------\n");
+    bzero(src1 + 4, 5);
+    printf("bzero: %s\n", src1);
+   ft_bzero(src2 + 4, 5);
+    printf("ft_bzero: %s\n", src2);
+}
