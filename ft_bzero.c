277d608b08dfdcb8a05e09d6309f4e40260b4195
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-08-23 10:04:31 +0200

    update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -11,15 +11,14 @@ void    ft_bzero(void *s, size_t n)
 
 #include <stdio.h>
 #include<string.h>
-
 int main(void)
 {
-    char src1[21] = "gdfhjgazGHJGgdhsqjjG";
-    char src2[21] = "gdfhjgazGHJGgdhsqjjG";
+    char src1[21] = "567:;2`$dfhjgazGHJGgdhsqjjG";
+    char src2[21] = "567:;2`$dfhjgazGHJGgdhsqjjG";
     printf("Before the function: %s\n", src1);
     printf("-------------------------------------------------\n");
-    bzero(src1 + 4, 5);
+    bzero(src1 + 7, 5);
     printf("bzero: %s\n", src1);
-   ft_bzero(src2 + 4, 5);
+   ft_bzero(src2 + 7, 5);
     printf("ft_bzero: %s\n", src2);
 }
