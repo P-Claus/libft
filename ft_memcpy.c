431eb4b6ee526b043427e4be757b65b2313a6eaf
Pieter Claus <pieter.claus1@gmail.com>
2023-08-25 18:03:47 +0200

    finish ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,9 +2,14 @@
 
 void    ft_memcpy(void *dest, void *src, size_t n)
 {
-    while ( n-- > 0)
-        *(char *)(dest + n) = *(char *)(src + n);
+	size_t	counter;
 
+	counter = 0;
+	while (counter < n)
+	{
+        	*(char *)(dest + counter) = *(char *)(src + counter);
+		counter++;
+	}
 }
 
 #include <stdlib.h>
@@ -12,41 +17,45 @@ void    ft_memcpy(void *dest, void *src, size_t n)
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_STRING_LENGTH 10
-#define NUMBER_OF_STRINGS 5
+#define MAX_STRING_LENGTH 20
+#define NUMBER_OF_STRINGS 10
 
 int main(void)
 {
-    srand(time(NULL));
-    
-    for (int loop = 0; loop < NUMBER_OF_STRINGS; loop++)
-    {
-        int string_length = rand() % MAX_STRING_LENGTH + 1;
-        char src[string_length + 1];
-        for (int i = 0; i < string_length; ++i)
-        {
-            if (rand() % 2 == 0)
-                src[i] = rand() % 26 + 'a';
-            else
-                src[i] = rand() % 26 + 'A';
-        }
-        src[string_length] = '\0';
-    
-    char dst1[string_length + 1];
-    char dst2[string_length + 1];
-    printf("Random string: %s\n", src);
-    printf("-------------------------------------------------\n");
-    printf("Before the function (dst 1): %s\n", dst1);
-    printf("Before the function (dst 2): %s\n", dst2);
-    printf("-------------------------------------------------\n");
-    memcpy(dst1, src, 10);
-    ft_memcpy(dst2, src, 10);
-    printf("After memcpy (dst 1): %s\n", dst1);
-    printf("After ft_memcpy (dst 2): %s\n", dst2);
-    printf("-------------------------------------------------\n");
-    printf("\n\n\n\n\n");
+	srand(time(NULL));
 
+	for (int loop = 0; loop < NUMBER_OF_STRINGS; loop++)
+	{
+		int string_length = rand() % MAX_STRING_LENGTH + 1;
+		char src[string_length + 1];
+		for (int i = 0; i < string_length; ++i)
+		{
+			if (rand() % 2 == 0)
+				src[i] = rand() % 26 + 'a';
+			else
+				src[i] = rand() % 26 + 'A';
+		}
+		src[string_length] = '\0';
     
+	char dst1[string_length + 1];
+	char dst2[string_length + 1];
+
+	for (int index = 0; index <= string_length; index++)
+	{
+		dst1[index] = '\0';
+		dst2[index] = '\0';
+	}
+	printf("Random string: %s\n", src);
+	printf("-------------------------------------------------\n");
+	printf("Before the function (dst 1): %s\n", dst1);
+	printf("Before the function (dst 2): %s\n", dst2);
+	printf("-------------------------------------------------\n");
+	memcpy(dst1, src, 15);
+	ft_memcpy(dst2, src, 15);
+	printf("After memcpy (dst 1): %s\n", dst1);
+	printf("After ft_memcpy (dst 2): %s\n", dst2);
+	printf("-------------------------------------------------\n");
+	printf("\n\n\n\n\n");
     }
 }
 
