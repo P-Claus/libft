e59354cddb0e702eb962ccce18ea041a284403d8
Pieter Claus <pieter.claus1@gmail.com>
2023-08-25 18:30:33 +0200

    finish ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,19 +1,22 @@
-size_t ft_strlen(const char *s)
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s)
 {
-    size_t  len;
-    
-    len = 0;
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    return (len);
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
 
 /*
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define MAX_STRING_LENGTH 20
 #define NUM_STRINGS 5
@@ -24,9 +27,9 @@ int main()
 
     for (int loop = 0; loop < NUM_STRINGS; ++loop)
     {
-        int stringLength = rand() % MAX_STRING_LENGTH + 1; //create random string between 1 and max length
+        int stringLength = rand() % MAX_STRING_LENGTH + 1;
         char randomString[stringLength + 1];
-        for (int i = 0; i < stringLength; ++i) //start generating letters
+        for (int i = 0; i < stringLength; ++i)
         {
             if (rand() % 2 == 0)
                 randomString[i] = rand() % 26 + 'a';
@@ -37,7 +40,7 @@ int main()
         printf("-------------------------------------------------\n");
         printf("RANDOM STRING %d: %s\n", loop + 1, randomString);
         printf("ft_strlen: %zu\n", ft_strlen(randomString));
-        printf("strlen: %zu\n", strlen(randomString);
+        printf("strlen: %zu\n", strlen(randomString));
         printf("-------------------------------------------------\n");
         printf("\n\n\n\n\n");
     }
