c5a2ecd26d647de4ec78c9ba87b990a7835975ae
Pieter Claus <pieter.claus1@gmail.com>
2023-10-18 13:49:55 +0200

    update ft_memcpy.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:15:12 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/17 18:59:35 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/18 13:46:12 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_isascii(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	ft_memcpy(void *dst, void *src, size_t n);
+void	ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 
 
--- libft/srcs/ft_memcpy.c
+++ libft/srcs/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 14:12:28 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/17 14:14:06 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/18 13:47:55 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memcpy(void *dest, void *src, size_t n)
+void	ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	counter;
 
