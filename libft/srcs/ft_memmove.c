2c23a5f5c439f12093581e5a73255b3f1f0b9268
Pieter Claus <pieter.claus1@gmail.com>
2023-10-18 13:37:30 +0200

    finish ft_memmove.c

--- libft/srcs/ft_memmove.c
+++ libft/srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:37:51 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/17 22:47:24 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/18 13:34:34 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		counter = len;
 		while (counter > 0)
 		{
-			--counter;
-			((unsigned char *) dst)[counter] = ((unsigned char *) dst)[counter];
+			counter--;
+			*(unsigned char *)(dst + counter) = \
+				*(unsigned char *)(src + counter);
 		}
 	}
 	else
@@ -32,31 +33,10 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		counter = 0;
 		while (counter < len)
 		{
-			((unsigned char *) dst)[counter] = ((unsigned char *) src)[counter];
+			*(unsigned char *)(dst + counter) = \
+				*(unsigned char *)(src + counter);
 			counter++;
 		}
-	}	
+	}
 	return (dst);
-}
-
-#include <string.h>
-#include <stdio.h>
-int	main(void)
-{
-	char	s1[] = "He is the boss of the whole company";
-	char	s2[] = "He is the boss of the whole company";
-	char	s3[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-	char	s4[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-
-	printf("String 1: %s\n", s1);
-	printf("String 2: %s\n", s2);
-	printf("String 3: %s\n", s3);
-	printf("String 4: %s\n", s4);
-	memmove(s3 + 5, s1, 14);	
-	ft_memmove(s4 + 5, s2, 14);	
-	printf("String 1 after memmove: %s\n", s1);
-	printf("String 2 after ft_memmove: %s\n", s2);
-	printf("String 3 after memmove: %s\n", s3);
-	printf("String 4 after ft_memmove: %s\n", s4);
-	
 }
--- main_functions/ft_memmove.c
+++ main_functions/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:37:51 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/17 22:09:35 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/18 13:33:39 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,45 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	temp[len];
-	size_t		counter;
+	size_t	counter;
 
-	counter = 0;
-	while (counter < len)
+	if (!dst && !src)
+		return (0);
+	if (src < dst)
 	{
-		*(char *)(temp + counter) = *(char *)(src + counter);
-		counter++;
+		counter = len;
+		while (counter > 0)
+		{
+			counter--;
+			*(unsigned char *)(dst + counter) = \
+				*(unsigned char *)(src + counter);
+		}
 	}
-	counter = 0;
-	while (counter < len)
+	else
 	{
-		*(char *)(dst + counter) = *(char *)(temp + counter);
-		counter++;
+		counter = 0;
+		while (counter < len)
+		{
+			*(unsigned char *)(dst + counter) = \
+				*(unsigned char *)(src + counter);
+			counter++;
+		}
 	}
 	return (dst);
-	
 }
 /*
 #include <string.h>
 #include <stdio.h>
 int	main(void)
 {
-	char	s1[] = "He is the boss of the whole company";
-	char	s2[] = "He is the boss of the whole company";
-	char	s3[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-	char	s4[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	char	s1[] = "hellomyfriend";
+	char	s2[] = "hellomyfriend";
 
 	printf("String 1: %s\n", s1);
 	printf("String 2: %s\n", s2);
-	printf("String 3: %s\n", s3);
-	printf("String 4: %s\n", s4);
-	memmove(s3 + 5, s1, 14);	
-	ft_memmove(s4 + 5, s2, 14);	
+	memmove(s1 + 3, s1, 5);	
+	ft_memmove(s2 + 3, s2, 5);	
 	printf("String 1 after memmove: %s\n", s1);
 	printf("String 2 after ft_memmove: %s\n", s2);
-	printf("String 3 after memmove: %s\n", s3);
-	printf("String 4 after ft_memmove: %s\n", s4);
 	
 }*/
