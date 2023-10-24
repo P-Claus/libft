71751b8d4ce26151a62ac410d80144816c3a007e
Pieter Claus <pieter.claus1@gmail.com>
2023-10-24 21:59:11 +0200

    finish ex ft_putchar_fd.c

--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:15:12 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/24 14:38:47 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/24 21:58:11 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	ft_bzero(void *s, size_t n);
 void	ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_calloc(size_t num_elements, size_t element_size);
+void    ft_putchar_fd(char c, int fd);
 char    *ft_strchr(const char *str, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strdup(const char *str);
--- main_functions/ft_putchar_fd.c
+++ main_functions/ft_putchar_fd.c
@@ -6,13 +6,22 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 14:29:48 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/24 21:56:31 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/24 21:54:10 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <fcntl.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
+}
+
+int	main(void)
+{
+	char	c = 'n';
+	int	fd = open("text.txt", O_WRONLY);
+	ft_putchar_fd(c, fd);
+	return (0);
 }
