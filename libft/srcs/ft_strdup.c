61525434d060b993302fabb2715902796aada165
Pieter Claus <pieter.claus1@gmail.com>
2023-10-26 18:55:49 +0200

    finish ft_strtrim.c, updated ft_strdup.c and added makefile

--- libft/srcs/ft_strdup.c
+++ libft/srcs/ft_strdup.c
@@ -6,29 +6,19 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 17:18:19 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/21 17:35:14 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/26 18:14:54 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_calculate_length(char *str)
-{
-	int	len;
-
-	len = 0;
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *src)
 {
 	int		len;
 	int		counter;
 	char	*dst;
 
-	len = ft_calculate_length(src) + 1;
+	len = ft_strlen(src) + 1;
 	counter = 0;
 	dst = (char *) malloc(len * sizeof(char));
 	if (dst == 0)
