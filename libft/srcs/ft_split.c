43e523c3bc6af6d4607e6717aa8602f30c4b05ea
Pieter Claus <pieter.claus1@gmail.com>
2023-10-27 21:53:12 +0200

    started work on ft_split

--- libft/srcs/ft_split.c
+++ libft/srcs/ft_split.c
@@ -6,11 +6,20 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:31:23 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/27 17:41:20 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/27 21:52:30 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	check_delimiter(char c, char del)
+{
+	if (c == del)
+		return (1);
+	if (c == 0)
+		return (0);
+	return (0);
+}
 
 char	**ft_split(char const *s, char c)
 {
