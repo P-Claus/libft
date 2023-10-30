a3961d27b0adbbe312b8498ff3168aff94a8fa20
Pieter Claus <pclaus@a2-r3-p7.s19.be>
2023-10-30 16:57:53 +0100

    work on ft_strmapi.c

--- libft/srcs/ft_strmapi.c
+++ libft/srcs/ft_strmapi.c
@@ -6,13 +6,13 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:03:54 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/30 09:31:11 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/30 16:53:43 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int), char)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	int		index;
 	int		len;
@@ -20,22 +20,35 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int), char)
 
 	index = 0;
 	len = ft_strlen(s);
-	if (!(str = (char *)malloc((len - 1) * sizeof(char))));
-		return (0);
-	
-}
-
-char f(unsigned int i, char c)
-{
-	char str;
-	str = c + 1;
+	str = (char *) malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (index < len)
+	{
+		str[index] = f(index, s[index]);
+		index++;
+	}
+	str[index] = '\0';
 	return (str);
 }
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+char	f(unsigned int i, char c)
+{
+	char	*str;
+
+	i = 0;
+	str = NULL;
+	str[i] = c + '5';
+	return (*str);
+}
+
+int	main(void)
 {
 	char str1[] = "abc";
 	char* str2;
-	str2 = ft_strmapi(str1, *f);
+	str2 = ft_strmapi(str1, f);
 	printf("%s\n", str2);
+	free(str2);
 }
