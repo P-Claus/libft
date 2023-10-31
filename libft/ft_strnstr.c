993574105f8a702ad1e67d1b65084636348a610b
Pieter Claus <pieter.claus1@gmail.com>
2023-10-31 19:41:49 +0100

    finish ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 15:41:06 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/31 18:59:40 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/31 19:41:23 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	h = 0;
 	n = 0;
-	if (!needle)
-		return (char *)(haystack);
-	while (haystack[h] != '\0')
+	if (!*needle)
+		return ((char *)haystack);
+	while (haystack[h])
 	{
-		while ((haystack[h + n]) == needle[n] \
-				&& (h + n) < len)
+		n = 0;
+		while (haystack[h] == needle[n] && haystack[h] && h < len)
 		{
-			if (haystack[h + n] == '\0' && needle[n] == '\0')
-				return ((char *)&haystack[h]);
+			h++;
 			n++;
 		}
 		if (needle[n] == '\0')
-			return ((char *)(haystack + h ));
-		h++;
+			return ((char *)&haystack[h - n]);
+		h = (h - n) + 1;
 	}
 	return (0);
 }
-/*
-#include <stdio.h>
-
-char *ft_strnstr(const char *haystack, const char *needle, size_t len);
-
-int main() {
-    const char *haystack = "This is a sample string for testing.";
-    const char *needle = "sample";
-    size_t len = 20; // The maximum number of characters to search in haystack
-
-    char *result = ft_strnstr(haystack, needle, len);
-
-    if (result) {
-        printf("Needle found: %s\n", result);
-    } else {
-        printf("Needle not found.\n");
-    }
-
-    return 0;
-}
-*/
