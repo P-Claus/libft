afe7310b1efe56a8ae4115d43df798ca2992d694
Pieter Claus <pieter.claus1@gmail.com>
2023-10-31 13:47:41 +0100

    modified and finished ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:00:44 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/18 14:17:38 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/31 13:46:23 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	len;
 
 	len = 0;
-	while (src[len] != '\0' && len + 1 < dstsize)
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (src[len] != '\0' && len < dstsize - 1)
 	{
 		dst[len] = src[len];
 		len++;
 	}
-	dst[len] = '\0';
+	if (len < dstsize)
+		dst[len] = '\0';
 	while (src[len] != '\0')
 		len++;
 	return (len);
