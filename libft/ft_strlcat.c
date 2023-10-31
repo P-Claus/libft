b1a6ba954541b5c99506d166e09aaa9b84772989
Pieter Claus <pieter.claus1@gmail.com>
2023-10-31 14:12:23 +0100

    finish ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,39 +6,27 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:22:28 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/18 22:00:46 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/31 14:07:09 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_calculate_length(const char *str)
-{
-	int	len;
-
-	len = 0;
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dst_len;
-	int		src_len;
-	int		src_index;
-	int		dst_index;
+	size_t	dst_index;
+	size_t	src_index;
 
-	dst_len = ft_calculate_length(dst);
-	src_len = ft_calculate_length(src);
+	dst_index = 0;
 	src_index = 0;
-	dst_index = dst_len;
-	while (src [src_index] != '\0' && dst_index < (int) size - 1)
-	{
-		dst[dst_index] = src[src_index];
+	while (dst[dst_index] && dst_index < size)
 		dst_index++;
+	while (src[src_index] && (dst_index + src_index + 1) < size)
+	{
+		dst[dst_index + src_index] = src[src_index];
 		src_index++;
 	}
-	dst[dst_index] = '\0';
-	return (dst_len + src_len);
+	if (dst_index < size)
+		dst[dst_index + src_index] = '\0';
+	return (dst_index + ft_strlen(src));
 }
