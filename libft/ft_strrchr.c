b71e3214fd8dd55b6ea3f16219712d026bc0f979
Pieter Claus <pieter.claus1@gmail.com>
2023-11-01 17:09:45 +0100

    finish ft_strrchr.c

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 18:18:58 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/01 09:05:41 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/01 17:09:26 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,13 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int	index;
-	int	match;
 
-	index = 0;
-	match = -1;
-	while (s[index] != '\0')
+	index = ft_strlen(s);
+	while (index >= 0)
 	{
 		if (s[index] == c)
-			match = index;
-		index++;
+			return ((char *)&s[index]);
+		index--;
 	}
-	if (c == '\0')
-		return ((char *)&s[index]);
-	if (match == -1)
-		return (0);
-	return ((char *)&s[match]);
+	return (0);
 }
