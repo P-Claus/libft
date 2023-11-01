3b081afb36ace160c0391dd133f5af9942caf53a
Pieter Claus <pieter.claus1@gmail.com>
2023-11-01 15:36:26 +0100

    finish ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 14:12:28 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/31 12:06:09 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/01 15:35:01 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	counter;
 
 	counter = 0;
+	if (!dest && !src)
+		return (0);
 	while (counter < n)
 	{
-		*(char *)(dest + counter) = *(char *)(src + counter);
+		*(unsigned char *)(dest + counter) = *(unsigned char *)(src + counter);
 		counter++;
 	}
 	return (dest);
