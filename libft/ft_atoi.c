4ea4f64e55a2ec7a106aa74eaf02fbd4fb0e8634
Pieter Claus <pieter.claus1@gmail.com>
2023-11-01 09:36:59 +0100

    finish ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:21:59 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/20 18:11:12 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/01 09:33:18 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,18 @@ int	ft_atoi(const char *str)
 	iter = 0;
 	sign = 1;
 	result = 0;
-	while ((str[iter] == 32) || (str[iter] >= 9 && str[iter] <= 13))
+	while (str[iter] && ((str[iter] >= 9 && str[iter] <= 13) || \
+				str[iter] == 32))
 		iter++;
-	if (str[iter] == '+')
-		iter++;
-	else if (str[iter] == '-')
+	if (str[iter] == '+' || str[iter] == '-')
 	{
-		sign = -1;
+		if (str[iter] == '-')
+			sign = -1;
 		iter++;
 	}
-	while (str[iter] != '\0' && str[iter] >= 48 && str[iter] <= 58)
+	while (str[iter] >= '0' && str[iter] <= '9' && str[iter] != '\0')
 	{
-		result *= 10;
-		result += str[iter] - '0';
+		result = result * 10 + str[iter] - '0';
 		iter++;
 	}
 	return (result * sign);
