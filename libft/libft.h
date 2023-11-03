e878f9ae29c2047014ba5c99fea83f03f1c60400
Pieter Claus <pieter.claus1@gmail.com>
2023-11-03 17:35:40 +0100

    finish ft_lstclear.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:15:12 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/03 16:57:21 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/03 17:31:01 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,4 +74,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void(*del)(void*));
+void	ft_lstclear(t_list **lst, void(*del)(void*));
+
 #endif
