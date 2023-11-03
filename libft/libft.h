d6ceee7369497b1e3d78c3a8810299f64a51889f
Pieter Claus <pclaus@a2-r4-p10.s19.be>
2023-11-03 12:53:01 +0100

    finish ft_lstadd_back.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:15:12 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/02 21:58:14 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/03 11:37:40 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-void    ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
--- main_functions/ft_lstadd_back.c
+++ main_functions/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:48:54 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/03 12:52:29 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/03 11:36:54 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,42 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	}
 	current->next = new;
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	value1 = 46;
+	int	value2 = 79;
+	int	value3 = 18;
+	int	value4 = 65;
+
+	t_list *new1 = ft_lstnew(&value1);
+	t_list *new2 = ft_lstnew(&value2);
+	t_list *new3 = ft_lstnew(&value3);
+	t_list *new4 = ft_lstnew(&value4);
+	new1->next = new2;
+
+	t_list *current = new1;  
+	while (current != NULL)
+	{
+        	printf("Node content: %d\n", *(int *)current->content);
+        	current = current->next;
+    	}
+
+    	printf("Now we run the ft_lstadd_back function...\n");
+	ft_lstadd_back(&new1, new3);
+	ft_lstadd_back(&new1, new4);
+	
+	t_list *ncurrent = new1;
+	while (ncurrent != NULL)
+	{
+        	printf("Node content: %d\n", *(int *)ncurrent->content);
+        	ncurrent = ncurrent->next;
+ 	}
+
+
+	free(new1);
+	free(new2);
+	free(new3);
+	free(new4);
+}*/
