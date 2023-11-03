d967cce8c4709771a7f97a795836b7a83b44a2a6
Pieter Claus <pieter.claus1@gmail.com>
2023-11-03 18:25:26 +0100

    update typecast result

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 21:49:25 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/01 15:39:05 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/03 18:24:05 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (index < n)
 	{
 		if (*str == character)
-			return (str);
+			return (void *)(str);
 		index++;
 		str++;
 	}
