858b133f0263bce1c7ff092e9788467de80c29aa
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2023-11-10 13:13:46 +0100

    update main_test.c

--- main_functions/main_test.c
+++ main_functions/main_test.c
@@ -22,7 +22,6 @@ int main() {
     }
     printf("\n\n\n\n\n");
     
-    
     //ft_memcpy
     printf("-------------------ft_memcpy----------------------\n");
     char source[] = "Hello, World!";
@@ -32,4 +31,36 @@ int main() {
     printf("Content of the destination array after using memcpy: %s\n", destination);
     printf("\n\n\n\n\n");
 
+    //ft_memmove
+    printf("------------------ft_memmove---------------------\n");
+    char source2[] = "Hello, World!";
+    char destination2[20];
+    printf("Initial content of the destination array: %s\n", destination2);
+    memmove(destination2, source2, sizeof(source2));
+    printf("Content of the destination array after using memmove: %s\n", destination2);
+    printf("\n\n\n\n\n");
+    
+    //strlcpy
+    printf("------------------ft_strlcpy---------------------\n");
+    const char *source3 = "Hello, World!";
+    char destination3[20];
+    size_t copied3 = strlcpy(destination3, source3, sizeof(destination3));
+    printf("Content of the destination buffer: %s\n", destination3);
+    printf("Number of characters copied: %zu\n", copied3);
+    printf("\n\n\n\n\n");
+
+
+    //ft_strlcat
+    printf("------------------ft_strlcat---------------------\n");
+    char destination4[20] = "Hello, ";
+    const char *source4 = "World!";
+    size_t destination_size4 = sizeof(destination4);
+
+    printf("Original destination: %s\n", destination4);
+
+    size_t result4 = strlcat(destination4, source4, destination_size4);
+
+    printf("Concatenated string: %s\n", destination4);
+    printf("Total length after concatenation: %zu\n", result4);
+    printf("\n\n\n\n\n");
 }
