e7ac4a2e2902b40c749803a0394cbff5a1beb988
Pieter Claus <90153611+P-Claus@users.noreply.github.com>
2024-01-26 11:31:10 +0100

    Add files via upload

---
+++ libft/ft_putstr_color_fd.c
@@ -0,0 +1,12 @@
+# define ANSI_COLOR_RED		"\x1b[31m"
+# define ANSI_COLOR_BLUE	"\x1b[34m"
+# define ANSI_COLOR_GREEN	"\x1b[32m"
+# define ANSI_COLOR_YELLOW	"\x1b[33m"
+# define ANSI_COLOR_RESET	"\x1b[0m"
+
+void	ft_putstr_color_fd(char *color, char *s, int fd)
+{
+	ft_putstr_fd(color, fd);
+	ft_putstr_fd(s, fd);
+	ft_putstr_fd(ANSI_COLOR_RESET, fd);
+}
