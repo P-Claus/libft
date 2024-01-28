0b63f84a4c758ddccf4d4be3c87082577d2920a3
Pieter Claus <pieter.claus1@gmail.com>
2024-01-28 16:42:51 +0100

    put .h file in includes folder

--- libft/print_hex_upper.c
+++ libft/print_hex_upper.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "includes/ft_printf.h"
 
 int	print_hex_upper(long n, int base)
 {
